1979ede0fc5404acb1c8801ddfc4e2f119148c48
JuvnMillx <juanma.pedrosalopez@gmail.com>
2018-06-04 03:40:54 +0200

    Creación de SplashScreen, anteriormente borrado por error.

--- app/src/main/java/project/welltec/rankitapp/SplashScreen.java
+++ app/src/main/java/project/welltec/rankitapp/SplashScreen.java
@@ -1,13 +1,35 @@
 package project.welltec.rankitapp;
 
+import android.content.pm.ActivityInfo;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import project.welltec.rankitapp.utils.Constants;
+
 public class SplashScreen extends AppCompatActivity {
+
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+        // Transition to HomeActivity
+        countDownTimer = new CountDownTimer(Constants.DEFAULTTMETRANSITION, Constants.INTERVALTIMER ) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
     }
 }
---
+++ app/src/main/java/project/welltec/rankitapp/utils/Constants.java
@@ -0,0 +1,6 @@
+package project.welltec.rankitapp.utils;
+
+public class Constants {
+    public static final long DEFAULTTMETRANSITION = 3000;
+    public static final long INTERVALTIMER = 300;
+}
