43c8b30e80dd87f60894201d51237da710f9b197
Juanma <juanma.pedrosalopez@gmail.com>
2018-06-08 03:08:53 +0200

    Creado layout 'custom_actionbar'. Añadido estilo del ActionBar. Clase para manejo del ActionBar.

---
+++ app/src/main/java/project/welltec/rankitapp/utils/ActionBarManagement.java
@@ -0,0 +1,26 @@
+package project.welltec.rankitapp.utils;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import project.welltec.rankitapp.R;
+
+public final class ActionBarManagement {
+    private static ActionBar myActionBar = null;
+    private static LayoutInflater myLayoutInflater = null;
+
+    public static void inflateActionBar(ActionBar actionBar, LayoutInflater layoutInflater){
+        //Get the default actionbar instance
+        myActionBar = actionBar;
+        myActionBar.setDisplayShowHomeEnabled(false);
+        myActionBar.setDisplayShowTitleEnabled(false);
+
+        //Initializes the custom action bar layout
+        myLayoutInflater = layoutInflater;
+        View mCustomView = myLayoutInflater.inflate(R.layout.custom_actionbar, null);
+        myActionBar.setCustomView(mCustomView);
+        myActionBar.setDisplayShowCustomEnabled(true);
+    }
+}
